647acf3f6e135790fdeac91363e637e118fc5ae7
James Wagoner <jameswagoner@gmail.com>
2020-07-07 20:12:43 -0700

    Morgan logger

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
+const logger = require('morgan')
 
 const tenantRoutes = require('./routes/tenant');
+
+app.use(logger('dev'))
 
 app.use('/tenants', tenantRoutes)
 
