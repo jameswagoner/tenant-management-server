be82b6f78bacad18b37fce265f3e1c426706eca9
James Wagoner <jameswagoner@gmail.com>
2020-07-07 19:49:42 -0700

    Empty tenant route

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
 
-app.use((req, res, next) => {
-  res.status(200).send('Silence is golden')
-})
+const tenantRoutes = require('./routes/tenant');
+
+app.use('/tenants', tenantRoutes)
 
 module.exports = app
