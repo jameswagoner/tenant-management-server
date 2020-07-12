c44935ab320da6126a1648621b5818a25e81e244
James Wagoner <jameswagoner@gmail.com>
2020-07-11 21:47:48 -0700

    WIP: Authentication

---
+++ server/Middleware/Authenticated.js
@@ -0,0 +1,33 @@
+const { verify } = require('jsonwebtoken')
+
+module.exports = (req, res, next) => {
+  const authHeader = req.headers.authorization
+
+  if (!authHeader) {
+    return res.status(403).json({
+      status: 403,
+      message: 'FORBIDDEN'
+    })
+  } else {
+    const token = authHeader.split(' ')[1]
+
+    if (token) {
+      return verify(token)
+        .then(user => {
+          res.user = user
+          next()
+        })
+        .catch((err) => {
+          return res.status(401).json({
+            status: 401,
+            message: 'UNAUTHORIZED'
+          })
+        })
+    } else {
+      return res.status(403).json({
+        status: 403,
+        message: 'FORBIDDEN'
+      })
+    }
+  }
+}
