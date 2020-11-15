3e317b64cb3751a9b4d681f8501fec71ffdbd438
James Wagoner <jameswagoner@gmail.com>
2020-11-14 19:15:09 -0800

    decoupling TaskService

--- server/Controllers/TaskController.js
+++ server/Controllers/TaskController.js
@@ -1,7 +1,22 @@
 const TaskService = require('../Services/TaskService');
 
 module.exports = {
-  index: TaskService.all,
-  show: TaskService.find,
-  store: TaskService.create
+  index: (req, res) => {
+    TaskService.all()
+      .then(tasks => res.json(tasks));
+  },
+  show: (req, res) => {
+    TaskService.find(req.params.id)
+      .then(task => {
+        if (task) {
+          res.json(task);
+        } else {
+          res.status(404).send();
+        }
+      });
+  },
+  store: (req, res) => {
+    TaskService.create(req.params)
+      .then(task => res.json(task));
+  }
 }
--- server/Services/TaskService.js
+++ server/Services/TaskService.js
@@ -2,51 +2,18 @@ const { model } = require('../Models/Task');
 const Task = model;
 
 module.exports = {
-  all: (req, res) => {
-    Task.find()
-      .then(tasks => {
-        res.status(200).json({
-          tasks: tasks
-        })
-      })
-      .catch(err => {
-        res.status(err.status || 500);
-        res.json({
-          error: err
-        });
-      });
+  all: () => {
+    return Task.find();
   },
-  find: (req, res) => {
-    Task.findById(req.params.task)
-      .then(task => {
-        if (task) {
-          res.status(200).json(task)
-        } else {
-          res.status(404).json();
-        }
-      })
-      .catch(err => {
-        res.status(err.status || 500);
-        res.json({
-          error: err
-        });
-      });
+  find: (id) => {
+    return Task.findById(id);
   },
-  create: (req, res) => {
-    Task.create({
-        title: req.params.title,
-        description: req.params.description,
-        unit: req.params.unit,
-        priority: req.params.priority
-      })
-      .then(task => {
-        res.status(200).json(task)
-      })
-      .catch(err => {
-        res.status(err.status || 500);
-        res.json({
-          error: err
-        });
+  create: (input) => {
+    return Task.create({
+        title: input.title,
+        description: input.description,
+        unit: input.unit,
+        priority: input.priority
       });
   }
 };
