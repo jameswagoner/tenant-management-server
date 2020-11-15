ea84ce814b8b1ec5ffa9703116997643f514dc99
James Wagoner <jameswagoner@gmail.com>
2020-11-14 23:30:23 -0800

    removing tenant stuff

--- server/Controllers/TenantController.js
+++
@@ -1,8 +0,0 @@
-const TenantService = require('../Services/TenantService');
-
-module.exports = {
-  index: TenantService.all,
-  show: TenantService.find,
-  store: TenantService.create,
-  invoices: TenantService.invoices
-}
